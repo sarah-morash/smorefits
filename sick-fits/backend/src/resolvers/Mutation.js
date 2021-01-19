351caa33a3eeb849d2b194cb22d5fe5fe2f32bb8
Sarah Morash <sarahjmorash@gmail.com>
2021-01-19 15:59:40 -0400

    Updated initial resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,16 @@
 const Mutation = {
-  //   createDog(parent, args, ctx, info) {
-  //     global.dogs = global.dogs || [];
-  //     // create a dog
-  //     const newDog = { name: args.name };
-  //     global.dogs.push(newDog);
-  //     return newDog;
-  //   }
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
 };
 
 module.exports = Mutation;
